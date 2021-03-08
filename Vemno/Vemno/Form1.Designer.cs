b543098c052744f3dc24c8f242df3dd72372acb7
MarBarBoyce <78444110+MarleneBoyce@users.noreply.github.com>
2021-03-08 12:46:55 -0800

    Add files via upload
    Updated Project files (final)

---
+++ Vemno/Vemno/Form1.Designer.cs
@@ -0,0 +1,319 @@
+﻿namespace Vemno
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.txtOutput = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.btnName = new System.Windows.Forms.Button();
+            this.btnPayment = new System.Windows.Forms.Button();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtAmount = new System.Windows.Forms.TextBox();
+            this.txtReason = new System.Windows.Forms.TextBox();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.txtStarts = new System.Windows.Forms.TextBox();
+            this.txtEnds = new System.Windows.Forms.TextBox();
+            this.btnSearch = new System.Windows.Forms.Button();
+            this.btnStartsW = new System.Windows.Forms.Button();
+            this.btnEndsW = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(511, 50);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(80, 25);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Vemno";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(61, 274);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(206, 25);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Reason for Payment";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(61, 564);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(158, 25);
+            this.label3.TabIndex = 0;
+            this.label3.Text = "Search Results";
+            // 
+            // txtOutput
+            // 
+            this.txtOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtOutput.Location = new System.Drawing.Point(61, 609);
+            this.txtOutput.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtOutput.Multiline = true;
+            this.txtOutput.Name = "txtOutput";
+            this.txtOutput.ReadOnly = true;
+            this.txtOutput.Size = new System.Drawing.Size(975, 265);
+            this.txtOutput.TabIndex = 1;
+            this.txtOutput.TabStop = false;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(61, 121);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(174, 25);
+            this.label4.TabIndex = 0;
+            this.label4.Text = "Recipent\'s Name";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(61, 229);
+            this.label7.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(167, 25);
+            this.label7.TabIndex = 0;
+            this.label7.Text = "Amount to Send";
+            // 
+            // btnName
+            // 
+            this.btnName.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnName.Location = new System.Drawing.Point(437, 156);
+            this.btnName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnName.Name = "btnName";
+            this.btnName.Size = new System.Drawing.Size(233, 37);
+            this.btnName.TabIndex = 1;
+            this.btnName.Text = "Enter Name";
+            this.btnName.UseVisualStyleBackColor = true;
+            this.btnName.Click += new System.EventHandler(this.btnName_Click);
+            // 
+            // btnPayment
+            // 
+            this.btnPayment.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPayment.Location = new System.Drawing.Point(437, 311);
+            this.btnPayment.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnPayment.Name = "btnPayment";
+            this.btnPayment.Size = new System.Drawing.Size(233, 37);
+            this.btnPayment.TabIndex = 4;
+            this.btnPayment.Text = "Enter Payment";
+            this.btnPayment.UseVisualStyleBackColor = true;
+            this.btnPayment.Click += new System.EventHandler(this.btnPayment_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(61, 505);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(111, 25);
+            this.label5.TabIndex = 0;
+            this.label5.Text = "Ends With";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(61, 449);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(108, 25);
+            this.label6.TabIndex = 0;
+            this.label6.Text = "Start With";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label8.Location = new System.Drawing.Point(61, 394);
+            this.label8.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(119, 25);
+            this.label8.TabIndex = 0;
+            this.label8.Text = "Search For";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(321, 119);
+            this.txtName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(432, 22);
+            this.txtName.TabIndex = 0;
+          
+            // 
+            // txtAmount
+            // 
+            this.txtAmount.Location = new System.Drawing.Point(321, 228);
+            this.txtAmount.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtAmount.Name = "txtAmount";
+            this.txtAmount.Size = new System.Drawing.Size(432, 22);
+            this.txtAmount.TabIndex = 2;
+            // 
+            // txtReason
+            // 
+            this.txtReason.Location = new System.Drawing.Point(321, 273);
+            this.txtReason.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtReason.Name = "txtReason";
+            this.txtReason.Size = new System.Drawing.Size(432, 22);
+            this.txtReason.TabIndex = 3;
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(215, 394);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(432, 22);
+            this.txtSearch.TabIndex = 5;
+            // 
+            // txtStarts
+            // 
+            this.txtStarts.Location = new System.Drawing.Point(215, 449);
+            this.txtStarts.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtStarts.Name = "txtStarts";
+            this.txtStarts.Size = new System.Drawing.Size(432, 22);
+            this.txtStarts.TabIndex = 7;
+            // 
+            // txtEnds
+            // 
+            this.txtEnds.Location = new System.Drawing.Point(215, 505);
+            this.txtEnds.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtEnds.Name = "txtEnds";
+            this.txtEnds.Size = new System.Drawing.Size(432, 22);
+            this.txtEnds.TabIndex = 9;
+            // 
+            // btnSearch
+            // 
+            this.btnSearch.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSearch.Location = new System.Drawing.Point(689, 388);
+            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSearch.Name = "btnSearch";
+            this.btnSearch.Size = new System.Drawing.Size(233, 37);
+            this.btnSearch.TabIndex = 6;
+            this.btnSearch.Text = "Submit";
+            this.btnSearch.UseVisualStyleBackColor = true;
+            this.btnSearch.Click += new System.EventHandler(this.btnSearch_Click);
+            // 
+            // btnStartsW
+            // 
+            this.btnStartsW.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStartsW.Location = new System.Drawing.Point(689, 443);
+            this.btnStartsW.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnStartsW.Name = "btnStartsW";
+            this.btnStartsW.Size = new System.Drawing.Size(233, 37);
+            this.btnStartsW.TabIndex = 8;
+            this.btnStartsW.Text = "Submit";
+            this.btnStartsW.UseVisualStyleBackColor = true;
+            this.btnStartsW.Click += new System.EventHandler(this.btnStartsW_Click);
+            // 
+            // btnEndsW
+            // 
+            this.btnEndsW.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnEndsW.Location = new System.Drawing.Point(689, 498);
+            this.btnEndsW.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnEndsW.Name = "btnEndsW";
+            this.btnEndsW.Size = new System.Drawing.Size(233, 37);
+            this.btnEndsW.TabIndex = 10;
+            this.btnEndsW.Text = "Submit";
+            this.btnEndsW.UseVisualStyleBackColor = true;
+            this.btnEndsW.Click += new System.EventHandler(this.btnEndsW_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1068, 907);
+            this.Controls.Add(this.txtEnds);
+            this.Controls.Add(this.txtStarts);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.txtReason);
+            this.Controls.Add(this.txtAmount);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnEndsW);
+            this.Controls.Add(this.btnStartsW);
+            this.Controls.Add(this.btnSearch);
+            this.Controls.Add(this.btnPayment);
+            this.Controls.Add(this.btnName);
+            this.Controls.Add(this.txtOutput);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "Form1";
+            this.Text = "Vemno";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox txtOutput;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Button btnName;
+        private System.Windows.Forms.Button btnPayment;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtAmount;
+        private System.Windows.Forms.TextBox txtReason;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.TextBox txtStarts;
+        private System.Windows.Forms.TextBox txtEnds;
+        private System.Windows.Forms.Button btnSearch;
+        private System.Windows.Forms.Button btnStartsW;
+        private System.Windows.Forms.Button btnEndsW;
+    }
+}
+
---
+++ Vemno/Vemno/Form1.cs
@@ -0,0 +1,238 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vemno
+{
+    public partial class Form1 : Form
+    {
+        // 7 March 2021 MBoyce NEW 6L Created Class Payee, included ID number to later join lists, and first name and last name 
+        class Payee
+        {
+            public int PayeeID { get; set; }
+            public string FirstName { get; set; }
+            public string LastName { get; set; }
+        }
+
+        // 7 March 2021 MBoyce NEW 6L Created Class PayInfo included ID number to later join lists, and Amount, reason AND payeeID 
+        class PayInfo
+        {
+            public int InfoID { get; set; }
+            public string Amount { get; set; }
+            public string Reason { get; set; }
+            public int PayeeID { get; set; }
+        }
+
+        // 7 March 2021 MBoyce NEW 2L Created 2 lists
+        private List<Payee> payees = new List<Payee>();
+        private List<PayInfo> info = new List<PayInfo>();
+
+        private int infoID, payeeID;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnName_Click(object sender, EventArgs e)
+        {
+            //7 March 2021 NEW 5L Error Handling. Enesures user inputs value 
+            if (string.IsNullOrEmpty(txtName.Text.Trim()))
+            {
+                txtOutput.Text = "Please enter the recipient's name.";
+                return;
+            }
+
+            //7 March 2021 MBoyce NEW 2L Trims spaces before and afer, then splits the string to seperate the first and last name 
+            txtName.Text.Trim();
+            string[] names = txtName.Text.Split(' ');
+
+
+            Payee payee = new Payee();
+
+            //7 March 2021 MBoyce NEW 1L Assigned ID number to each name has different ID
+            payee.PayeeID = ++payeeID;
+
+            //7 March 2021 MBoyce NEW 1L First name is inserted into index 0
+            payee.FirstName = names[0];
+
+            /*7 March 2021 MBoyce NEW Error Handling 4L. If user does not enter last name, 
+             * this will pass through the first name and a empty string for last name OR 
+             * if user enters middle name (multiple names), this will only allow first / last name. */
+
+            if (names.Count() == 1)
+                payee.LastName = string.Empty;
+            else
+                payee.LastName = names[names.Count()-1];
+
+
+            //7 March 2021 MBoyce NEW 1L Adds "payee" or (name) to payees list 
+            payees.Add(payee);
+
+            txtOutput.Text = "Saved recipient's name.";
+
+        }
+
+        private void btnPayment_Click(object sender, EventArgs e)
+        {
+            //7 March 2021 MBoyce NEW 7L Error Handling, user may not enter reason and payment without entering name 
+            if (payees.Count == 0 || string.IsNullOrEmpty(payees[payees.Count - 1].FirstName))
+            {
+                txtOutput.Text = "Please enter the recipient's name!!!";
+                txtAmount.Clear();
+                txtReason.Clear();
+                return;
+            }
+            // 7 March 2021 NEW 5L User must enter value for "amount" 
+            if (string.IsNullOrEmpty(txtAmount.Text.Trim()))
+            {
+                txtOutput.Text = "Please enter the amount to send.";
+                return;
+            }
+
+            // 7 March 2021 NEW 7L User must enter valid dollar amount 
+            decimal amount = 0;
+            if (decimal.TryParse(txtAmount.Text, out amount) == false)
+            {
+                txtOutput.Text = "Please enter a valid dollar amount.";
+                txtAmount.Clear();
+                return;
+            }
+            
+            //7 March 2021 NEW 5L User must enter reason 
+            if (string.IsNullOrEmpty(txtReason.Text.Trim()))
+            {
+                txtOutput.Text = "Please enter the reason you are sending money.";
+                return;
+            }
+
+            PayInfo payInfo = new PayInfo();
+
+            /*7 March 2021 MBoyce NEW 4L Incrementing infoID, assigning values to Amount and Reason,
+             * THEN ensuring the ID for the transaction is the same! Now the name and payment information has SAME ID*/
+
+            payInfo.InfoID = ++infoID;
+            payInfo.Amount = txtAmount.Text;
+            payInfo.Reason = txtReason.Text;
+            payInfo.PayeeID = payees.Last().PayeeID;
+
+            // 7 March 2021 MBoyce NEW 1L Adding payment information to list 
+            info.Add(payInfo);
+
+            txtOutput.Text = "Saved payment information...";
+
+            txtName.Clear();
+            txtAmount.Clear();
+            txtReason.Clear();
+
+            //txtName.ReadOnly = false;
+        }
+
+        private void btnSearch_Click(object sender, EventArgs e)
+        {
+            string search = txtSearch.Text.Trim();
+
+            // 7 March 2021 MBoyce NEW 14L Combined lists, then allowed search through by typing the beginning of the first/last name, amount, or reason. 
+            var query = info.Join(
+                payees,
+                info => info.InfoID,
+                payee => payee.PayeeID,
+                (info, payee) => new
+                {
+                    FirstName = payee.FirstName,
+                    LastName = payee.LastName,
+                    Amount = info.Amount,
+                    Reason = info.Reason,
+                }).Where(s => s.FirstName == search || 
+                              s.LastName == search ||
+                              s.Amount == search ||
+                              s.Reason == search);
+
+            txtOutput.Clear();
+
+            // 7 March 2021 MBoyce NEW 6L Output 
+            foreach (var res in query)
+            {
+                txtOutput.AppendText("Name: "+ res.FirstName + " " + res.LastName +
+                                     " - Amount Sent: " + string.Format("{0:c}", decimal.Parse(res.Amount)) + 
+                                     " - Reason: " + res.Reason + Environment.NewLine);
+            }
+
+            txtSearch.Clear();
+        }
+
+        private void btnStartsW_Click(object sender, EventArgs e)
+        {
+            string search = txtStarts.Text.Trim();
+
+            // 7 March 2021 MBoyce NEW 14L Combined lists, then allowed search through by typing the beginning of the first/last name, amount, or reason. 
+            var query = info.Join(
+                payees,
+                info => info.InfoID,
+                payee => payee.PayeeID,
+                (info, payee) => new
+                {
+                    FirstName = payee.FirstName,
+                    LastName = payee.LastName,
+                    Amount = info.Amount,
+                    Reason = info.Reason,
+                }).Where(s => s.FirstName.StartsWith(search) == true ||
+                              s.LastName.StartsWith(search) == true ||
+                              s.Amount.StartsWith(search) == true ||
+                              s.Reason.StartsWith(search) == true);
+
+            txtOutput.Clear();
+
+            // 7 March 2021 MBoyce NEW 6L Output 
+            foreach (var res in query)
+            {
+                txtOutput.AppendText("Name: " + res.FirstName + " " + res.LastName +
+                                     " - Amount Sent: " + string.Format("{0:c}", decimal.Parse(res.Amount)) +
+                                     " - Reason: " + res.Reason + Environment.NewLine);
+            }
+
+            txtStarts.Clear();
+        }
+
+
+        private void btnEndsW_Click(object sender, EventArgs e)
+        {
+            string search = txtEnds.Text.Trim();
+            // 7 March 2021 MBoyce NEW 14L Combined lists, then allowed search through by typing the beginning of the first/last name, amount, or reason. 
+            var query = info.Join(
+                payees,
+                info => info.InfoID,
+                payee => payee.PayeeID,
+                (info, payee) => new
+                {
+                    FirstName = payee.FirstName,
+                    LastName = payee.LastName,
+                    Amount = info.Amount,
+                    Reason = info.Reason,
+                }).Where(s => s.FirstName.EndsWith(search) == true ||
+                              s.LastName.EndsWith(search) == true ||
+                              s.Amount.EndsWith(search) == true ||
+                              s.Reason.EndsWith(search) == true);
+
+            txtOutput.Clear();
+
+
+            // 7 March 2021 MBoyce NEW 6L Output 
+            foreach (var res in query)
+            {
+                txtOutput.AppendText("Name: " + res.FirstName + " " + res.LastName +
+                                     " - Amount Sent: " + string.Format("{0:c}", decimal.Parse(res.Amount)) +
+                                     " - Reason: " + res.Reason + Environment.NewLine);
+            }
+
+            txtEnds.Clear();
+        }
+    
+    }
+}
